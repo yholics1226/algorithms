a87222a3aa951f59867da203a1839288dc19486b
yh411 <94048154+yholics1226@users.noreply.github.com>
2021-11-19 13:45:20 +0900

    [LeetCode] 461. Hamming Distance

--- L461.py
+++ L461.py
@@ -9,3 +9,12 @@ def hammingDistance(x, y):
         a = a // 2
         b = b // 2
     return answer
+
+# bit 연산 이용
+def solution(x, y):
+    res = (bin(x ^ y))[2:]
+    answer = 0
+    for x in res:
+        if x == '1':
+            answer += 1
+    return answer
