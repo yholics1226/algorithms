6eef7af7d7531418e0a45f490628fe362b1de2d3
yh411 <94048154+yholics1226@users.noreply.github.com>
2021-12-26 20:25:59 +0900

    [LeetCode] 973. K Closest Points to Origin

--- L973.py
+++ L973.py
@@ -9,3 +9,7 @@ def kClosest(points, k):
     for _ in range(k):
         answer.append(heapq.heappop(H)[1])
     return answer
+
+# another solution
+def kClosest(points, k):
+    return sorted(points, key=lambda x: x[0] * x[0] + x[1] * x[1])[:k]
