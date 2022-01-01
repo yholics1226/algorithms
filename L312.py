ba9721f7cb54c81c8ea626b841a28a4fcf5e01ce
yh411 <94048154+yholics1226@users.noreply.github.com>
2022-01-01 22:28:05 +0900

    [LeetCode] 312. Burst Balloons

---
+++ L312.py
@@ -0,0 +1,13 @@
+# [Burst Balloons] https://leetcode.com/problems/burst-balloons/
+
+def maxCoins(nums):
+    nums = [1] + nums + [1]
+    n = len(nums)
+    dp = [[0] * n for _ in range(n)]
+    for i in range(1, n-1):
+        for lt in range(1, n-i):
+            rt = lt + i - 1
+            for j in range(lt, rt+1):
+                dp[lt][rt] = max(dp[lt][rt], dp[lt][j-1] + nums[lt-1] * nums[j] * nums[rt+1] + dp[j+1][rt])
+    print(dp)
+    return dp[1][-2]
