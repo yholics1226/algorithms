130eb17fddea331b40fbd3679f1b640eff84639a
yh411 <94048154+yholics1226@users.noreply.github.com>
2022-01-03 15:19:18 +0900

    [백준] 20950. 미술가 미미

--- B20950.py
+++ B20950.py
@@ -29,3 +29,22 @@ def mix(s, k, lst):
             mix(i+1, k+1, lst + nums[i])
 mix(0, 0, [])
 print(ans)
+
+# python3 acceepted
+import sys
+input = sys.stdin.readline
+n = int(input())
+nums = [[*map(int, input().split())] for _ in range(n)]
+gom = [*map(int, input().split())]
+ans = 1000
+def solve(c, k, s, r, g, b):
+    global ans
+    if c == k:
+        diff = abs(r//k-gom[0]) + abs(g//k-gom[1]) + abs(b//k-gom[2])
+        ans = min(ans, diff)
+        return
+    for i in range(s, n):
+        solve(c+1, k, i+1, r+nums[i][0], g+nums[i][1], b+nums[i][2])
+for k in range(2, min(8, n+1)):
+    solve(0, k, 0, 0, 0, 0)
+print(ans)
