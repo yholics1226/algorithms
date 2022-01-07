28ef0307c33196b183477b8954a632e44f6c1441
yh411 <94048154+yholics1226@users.noreply.github.com>
2022-01-07 16:48:54 +0900

    [백준] 3107. IPv6

--- B3107.py
+++ B3107.py
@@ -2,14 +2,20 @@
 
 import sys
 ip = sys.stdin.readline().strip().split(':')
+if len(ip) > 8:
+    if ip[0] == '' and ip[1] == '':
+        ip = ip[1:]
+    else:
+        ip.pop()
 if ip[0] == '':
     ip[0] = '0000'
 if ip[-1] == '':
     ip[-1] = '0000'
 flag = 1 if len(ip) != 8 else 0
 for i in range(len(ip)):
-    if flag and len(ip[i]) == 0:
+    l = len(ip[i])
+    if flag and l == 0:
         ip[i] = '0000' + ':0000' * (8-len(ip))
-    elif len(ip[i]) < 4:
-        ip[i] = '0' * (4 - len(ip[i])) + ip[i]
+    elif l < 4:
+        ip[i] = '0' * (4 - l) + ip[i]
 print(':'.join(ip))
