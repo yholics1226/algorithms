4b9deeb499a4be653c0b85d610a2b9780efe6416
yh411 <94048154+yholics1226@users.noreply.github.com>
2022-01-23 23:27:43 +0900

    [백준] 1695. 팰린드롬 만들기

--- B1695.py
+++ B1695.py
@@ -27,3 +27,15 @@ for i in range(1, n):
         else:
             dp[j][j+i] = dp[j+1][j+i-1]
 print(dp[0][n-1])
+
+# refactor (accepted in Python3 - 7192 ms, 30860 KB)
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr = [*map(int, input().split())]
+dp = [[0] * n for _ in range(3)]
+for i in range(1, n):
+    for j in range(n-i):
+        dp[2][j] = dp[0][j+1] if arr[j] == arr[i+j] else 1 + min(dp[1][j], dp[1][j+1])
+    dp[0], dp[1] = dp[1].copy(), dp[2].copy()
+print(dp[1][0])
