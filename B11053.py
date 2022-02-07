d3e8d8ffb80dcc9d7de59b698e41aa854ac81939
yh411 <94048154+yholics1226@users.noreply.github.com>
2022-02-07 23:22:17 +0900

    [백준] 11053. 가장 긴 증가하는 부분 수열

---
+++ B11053.py
@@ -0,0 +1,12 @@
+# [가장 긴 증가하는 부분 수열] https://www.acmicpc.net/problem/11053
+
+import sys
+input = sys.stdin.readline
+n = int(input())
+s = [*map(int, input().split())]
+dp = [1] * n
+for i in range(1, n):
+    for j in range(i):
+        if s[i] > s[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(max(dp))
