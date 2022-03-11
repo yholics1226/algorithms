335ab97f515d80f00ede53d354adf50a2544e8d1
yh411 <94048154+yholics1226@users.noreply.github.com>
2022-03-11 15:07:10 +0900

    [프로그래머스] 로또의 최고 순위와 최저 순위 (Lv.1)

--- P77484.py
+++ P77484.py
@@ -1,9 +1,10 @@
 # [로또의 최고 순위와 최저 순위] https://programmers.co.kr/learn/courses/30/lessons/77484
 
 def solution(lottos, win_nums):
+    answer = [0, 0]
     rank = [6, 6, 5, 4, 3, 2, 1]
     cnt = len(set(lottos) & set(win_nums))
-    answer = [rank[cnt]]
+    answer[1] = rank[cnt]
     cnt += lottos.count(0)
-    answer.append(rank[cnt])
-    return answer[::-1]
+    answer[0] = rank[cnt]
+    return answer
