0e62dac726498f7223230565c2b57d5840ffb8bb
yh411 <94048154+yholics1226@users.noreply.github.com>
2022-03-11 22:16:39 +0900

    [프로그래머스] 완주하지 못한 선수 (Lv.1)

--- P42576.py
+++ P42576.py
@@ -1,10 +1,24 @@
 # [프로그래머스] 완주하지 못한 선수 (Lv.1)
 
+from collections import Counter
 def solution(participant, completion):
-    import collections
-    pH = collections.Counter(participant)
-    cH = collections.Counter(completion)
+    pH = Counter(participant)
+    cH = Counter(completion)
     for x in pH:
         if pH[x] != cH[x]:
             return x
-          
+
+# another solution (slower)
+from collections import Counter
+def solution(participant, completion):
+    res = Counter(participant) - Counter(completion)
+    return list(res.keys())[0]
+
+# using sort (slower)
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    completion.append('')
+    for p, c in zip(participant, completion):
+        if p != c:
+            return p
