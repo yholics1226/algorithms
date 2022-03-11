4fbbbbc99efe23f66fee43c5ebfa2f027de57591
yh411 <94048154+yholics1226@users.noreply.github.com>
2022-03-12 00:56:57 +0900

    [프로그래머스] 모의고사 (Lv.1)

--- P42840.py
+++ P42840.py
@@ -1,4 +1,4 @@
-# [프로그래머스] 모의고사 (Lv.1)
+# [모의고사] https://programmers.co.kr/learn/courses/30/lessons/42840
 
 def solution(answers):
     answer = []
@@ -6,19 +6,15 @@ def solution(answers):
     ans1 = [1, 2, 3, 4, 5]
     ans2 = [2, 1, 2, 3, 2, 4, 2, 5]
     ans3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-
-    for i in range(len(answers)):
-        if answers[i] == ans1[i % 5]:
+    for i, ans in enumerate(answers):
+        if ans == ans1[i % 5]:
             score[1] += 1
-        if answers[i] == ans2[i % 8]:
+        if ans == ans2[i % 8]:
             score[2] += 1
-        if answers[i] == ans3[i % 10]:
+        if ans == ans3[i % 10]:
             score[3] += 1
-
     m = max(score)
     for i in range(1, 4):
         if score[i] == m:
             answer.append(i)
-
     return answer
-    
