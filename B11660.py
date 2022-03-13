3a47a2d1e30c02dd2517e7505db72a0eb010e38f
yh411 <94048154+yholics1226@users.noreply.github.com>
2022-03-13 15:46:15 +0900

    [백준] 11660. 구간 합 구하기 5

---
+++ B11660.py
@@ -0,0 +1,28 @@
+# [구간 합 구하기 5] https://www.acmicpc.net/problem/11660
+
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+dp = [[0] * (n+1) for _ in range(n+1)]
+grid = [[*map(int, input().split())] for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        dp[i+1][j+1] = dp[i][j+1] + dp[i+1][j] - dp[i][j] + grid[i][j]
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1])
+
+# using itertools
+from itertools import accumulate
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+dp = [[0] * (n+1)]
+for _ in range(n):
+    dp.append([0] + list(accumulate(map(int, input().split()))))
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        dp[i][j] += dp[i-1][j]
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1])
