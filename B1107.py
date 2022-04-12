23849cc1314f8ca8195b6f6599a340664c189913
yh411 <94048154+yholics1226@users.noreply.github.com>
2022-04-13 00:12:31 +0900

    [백준] 1107. 리모컨

--- B1107.py
+++ B1107.py
@@ -9,10 +9,26 @@ if m == 0:
     print(min(len(str(n)), ans))
 else:
     broken = set(input().split())
-    for num in range(1000001): 
+    for num in range(max(0, n - ans), n + ans):
         for x in str(num):
             if x in broken:
                 break
         else:
             ans = min(ans, len(str(num)) + abs(n - num))
+    print(ans)
+
+# another expression
+import sys
+input = sys.stdin.readline
+n = int(input())
+m = int(input())
+ans = abs(n - 100)
+if m == 0:
+    print(min(len(str(n)), ans))
+else:
+    broken = set(input().split())
+    for num in range(max(0, n - ans), n + ans):
+        if set(str(num)) & broken:
+            continue
+        ans = min(ans, len(str(num)) + abs(n - num))
     print(ans)
