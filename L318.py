f5e071e7c2e5db64928bbe13c0ddf3b5d6f9ab62
yh411 <94048154+yholics1226@users.noreply.github.com>
2022-05-31 23:53:57 +0900

    [LeetCode] 318. Maximum Product of Word Lengths

--- L318.py
+++ L318.py
@@ -6,10 +6,45 @@ def maxProduct(words):
             if x in s2:
                 return False
         return True
-    answer = 0
-    for i in range(len(words)-1):
-        for j in range(i+1, len(words)):
+    n = len(words)
+    ans = 0
+    for i in range(n-1):
+        for j in range(i+1, n):
             if isUnique(words[i], words[j]):
-                tmp = len(words[i]) * len(words[j])
-                answer = max(answer, tmp)
-    return answer
+                ans = max(ans, len(words[i]) * len(words[j]))
+    return ans
+
+# using set operation
+def maxProduct(words):
+    n = len(words)
+    s = [set(word) for word in words]
+    ans = 0
+    for i in range(n-1):
+        for j in range(i+1, n):
+            if not (s[i] & s[j]):
+                ans = max(ans, len(words[i]) * len(words[j]))
+    return ans
+
+# using bit mask (faster)
+def maxProduct(words):
+    n = len(words)
+    mask = [0] * n
+    for i in range(n):
+        for c in set(words[i]):
+            mask[i] |= (1 << (ord(c) - 97))
+    ans = 0
+    for i in range(n-1):
+        for j in range(i+1, n):
+            if not mask[i] & mask[j]:
+                ans = max(ans, len(words[i]) * len(words[j]))
+    return ans
+
+# faster solution using bit mask
+def maxProduct(words):
+    d = {0: 0}
+    for word in words:
+        mask = 0
+        for c in set(word):
+            mask |= (1 << (ord(c) - 97))
+        d[mask] = max(d.get(mask, 0), len(word))
+    return max([d[x] * d[y] for x in d for y in d if not x & y])
