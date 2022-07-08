2ce12b2c4d4f3b49aaabf9eb0c9c462ace7ea45d
yh411 <94048154+yholics1226@users.noreply.github.com>
2022-07-08 23:45:41 +0900

    [LeetCode] 97. Interleaving String

--- L97.py
+++ L97.py
@@ -17,3 +17,20 @@ def isInterleave(s1, s2, s3):
         f.add((i, j))
         return False
     return dfs(0, 0)
+
+# memoization
+from collections import defaultdict
+def isInterleave(s1, s2, s3):
+    if len(s1) + len(s2) != len(s3):
+        return False
+    memo = defaultdict(lambda: -1)
+    def dfs(i, j):
+        if i == len(s1):
+            return s3[i+j:] == s2[j:]
+        if j == len(s2):
+            return s3[i+j:] == s1[i:]
+        if memo[(i, j)] != -1:
+            return memo[(i, j)]
+        memo[(i, j)] = ((s1[i] == s3[i+j]) and dfs(i+1, j)) or ((s2[j] == s3[i+j]) and dfs(i, j+1))
+        return memo[(i, j)]
+    return dfs(0, 0)
